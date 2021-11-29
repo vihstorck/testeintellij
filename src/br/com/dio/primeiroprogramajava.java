f205e2e214c28b3df711cdce65a36b098d5bf0c9
Vih Storck <vihstorck@gmail.com>
2021-11-29 16:36:25 -0300

    first commit

---
+++ src/br/com/dio/primeiroprogramajava.java
@@ -0,0 +1,17 @@
+package br.com.dio;
+
+import br.com.dio.model.gato;
+
+public class primeiroprogramajava {
+    public static void main(String[] args) {
+        gato gato_ = new gato();
+        System.out.println("Hello World!");
+        System.out.println(" ");
+        System.out.println("TESTE");
+        int a = 5;
+        int b = 15;
+        System.out.println("SOMA: " + (a + b));
+    }
+
+    /*ctrl + shift + / = comenta o bloco selecionado*/
+}
